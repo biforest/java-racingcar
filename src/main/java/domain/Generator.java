74d8054cedadb44deaf15a7a7ad562ae202b1f4e
Chae hee chan <eqholic1125@gmail.com>
2021-03-09 21:03:38 +0900

    feat: 난수 생성 기능 구현

---
+++ src/main/java/domain/Generator.java
@@ -0,0 +1,17 @@
+package domain;
+
+public class Generator {
+    private int MAX_LIMIT_NUMBER = 10; //default value
+
+    Generator() {
+
+    }
+
+    Generator(int maxNum) {
+        this.MAX_LIMIT_NUMBER = maxNum;
+    }
+
+    public int generateRandomNumber() {
+        return (int) ((Math.random() * 10000) % MAX_LIMIT_NUMBER);
+    }
+}
