7aaac17278dca6f58e3ed90d65409149b5fb53ec
Chae hee chan <eqholic1125@gmail.com>
2021-03-09 20:50:45 +0900

    feat: Validator클래스 구현

---
+++ src/main/java/domain/Validator.java
@@ -0,0 +1,108 @@
+package domain;
+
+import io.Printer;
+
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.List;
+import java.util.TreeSet;
+import java.util.regex.Pattern;
+
+public class Validator implements ValidatorInterface {
+    private static final int MAX_NAME_SIZE = 5;
+    private static final String valiNumber = "^[0-9]+$";
+    private static final String commaInARow = "^(,,)+$";
+    private static final String characterOTN = "^[a-zA-Z,]+$";
+    Printer printer;
+
+    public Validator() {
+        printer = new Printer();
+    }
+
+    @Override
+    public boolean validateName(String s) {
+        return inputNothing(s)
+                && inputCommaInARow(s)
+                && inputCharactersOtherThanName(s)
+                && startWithComma(s)
+                && endWithComma(s)
+                && over5Characters(s)
+                && inputSameName(s);
+    }
+
+    @Override
+    public boolean validateNumber(String s) {
+        return s.matches(valiNumber);
+    }
+
+    @Override
+    public boolean inputNothing(String s) {
+        if (s.equals("")) {
+            printer.printExceptionMessage("INPUT_NOTHING");
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public boolean inputCommaInARow(String s) {
+        if (Pattern.matches("^.*(,,).*+$", s)) { //체크 필요
+            printer.printExceptionMessage("INPUT_COMMA_IN_A_ROW");
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public boolean startWithComma(String s) {
+        if (s.charAt(0) == ',') {
+            printer.printExceptionMessage("START_WITH_COMMA");
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public boolean endWithComma(String s) {
+        if (s.charAt(s.length() - 1) == ',') {
+            printer.printExceptionMessage("END_WITH_COMMA");
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public boolean inputCharactersOtherThanName(String s) {
+        if (!Pattern.matches(characterOTN, s)) {
+            printer.printExceptionMessage("INPUT_CHARACTERS_OTHER_THAN_NAME");
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public boolean inputSameName(String s) {
+        List<String> CAR_NAME_LIST = Arrays.asList(s.split(","));
+        int sizeOfNameList = CAR_NAME_LIST.size();
+
+        for (int i = 0; i < sizeOfNameList; i++) {
+            if (CAR_NAME_LIST.subList(i + 1, sizeOfNameList).contains(CAR_NAME_LIST.get(i))) {
+                printer.printExceptionMessage("INPUT_SAME_NAME");
+                return false;
+            }
+        }
+        return true;
+    }
+
+    @Override
+    public boolean over5Characters(String s) {
+        String[] splitName = s.split(",");
+        for (String i : splitName)
+            if (i.length() > MAX_NAME_SIZE) {
+                printer.printExceptionMessage("OVER_5_CHARACTERS");
+                return false;
+            }
+        return true;
+    }
+
+}
