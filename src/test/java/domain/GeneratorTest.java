2c62d0c6794084afea920670f407ffd8477b70ab
Chae <eqholic1125@gmail.com>
2021-03-10 23:23:25 +0900

    test: Generator 테스트 케이스 작성

---
+++ src/test/java/domain/GeneratorTest.java
@@ -0,0 +1,20 @@
+package domain;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class GeneratorTest {
+    Generator generator;
+
+    @BeforeEach
+    void setUp() {
+        generator = new Generator(10);
+    }
+
+    @Test
+    void generateRandomNumberTest() {
+        assertThat(generator.generateRandomNumber()).isBetween(0, 9);
+    }
+}
