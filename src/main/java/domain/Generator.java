645be80f99b0a769ceed374f8e15ad95b5cea982
songpang <scc6920@naver.com>
2021-03-21 16:16:25 +0900

    refactor: 이번 미션에서 불필요한 생성자 제거

--- src/main/java/domain/Generator.java
+++ src/main/java/domain/Generator.java
@@ -1,14 +1,7 @@
 package domain;
 
 public class Generator {
-    private int MAX_LIMIT_NUMBER = 10; //default value
-
-    Generator() {}
-
-    Generator(int maxNum) {
-        this.MAX_LIMIT_NUMBER = maxNum;
-    }
-
+    private final int MAX_LIMIT_NUMBER = 10; //default value
     public int generateRandomNumber() {
         return (int) ((Math.random() * 10000) % MAX_LIMIT_NUMBER);
     }
