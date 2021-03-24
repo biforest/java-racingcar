82053fcc01be9b49b7947dee5b42e3a918d27a4b
DongMin-Lee <pkalsh345@gmail.com>
2021-03-24 15:20:53 +0900

    refactor: 책임 재분배를 위한 클래스 정리

--- src/main/java/racingcar/repository/exception/NotDuplicateNameException.java
+++
@@ -1,7 +0,0 @@
-package racingcar.repository.exception;
-
-public class NotDuplicateNameException extends RuntimeException {
-    public NotDuplicateNameException(String message) {
-        super(message);
-    }
-}
