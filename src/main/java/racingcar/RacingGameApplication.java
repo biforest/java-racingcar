92276f092f1a1d8bd3e5a67a7784743daf74c059
DongMin-Lee <pkalsh345@gmail.com>
2021-03-24 15:17:17 +0900

    refactor: RacingGameApplication의 책임 축소
    Before:
    - 게임의 순서 보장
    - 이 클래스 내에 절차 지향적인 코드가 집중되어 있었음
    - 다른 객체에게 명령하기 보다 메시지를 보내서 이 객체의 역할을 다 하는데에만 집중
    
    After:
    - 게임 한판 실행후 우승자 목록 출력

--- src/main/java/racingcar/RacingGameApplication.java
+++ src/main/java/racingcar/RacingGameApplication.java
@@ -1,89 +1,25 @@
 package racingcar;
 
-import racingcar.domain.Car;
-import racingcar.domain.exception.NotBlankException;
-import racingcar.dto.CarNumberDto;
-import racingcar.exception.NotZeroRoundException;
-import racingcar.repository.CarManager;
+import racingcar.domain.Racing;
+import racingcar.domain.round.Rounds;
+import racingcar.domain.Winner;
+import racingcar.domain.car.Cars;
 import racingcar.ui.Printer;
 import racingcar.ui.Receiver;
 
-import java.util.ArrayList;
-import java.util.InputMismatchException;
-import java.util.List;
 
 public class RacingGameApplication {
-    private static final int INPUT_ZERO = 0;
-    private static final String DELIMITER = ",";
 
     private final Printer printer = new Printer();
     private final Receiver receiver = new Receiver();
-    private final CarManager carManager = new CarManager();
 
     public void run() {
-        printer.requestCarName();
-        String line = receiver.receiveCarNames();
-        try {
-            List<Car> carList = createAllCars(splitInputLine(line));
-            carManager.addAllCars(carList);
+        Cars container = new Cars(receiver.receiveCarNames(printer));
+        Rounds rounds = new Rounds(receiver.receiveNumberOfRounds(printer));
 
-            printer.requestNumberOfRounds();
-            int rounds = receiveRounds();
-            proceedRound(rounds);
-
-            printer.printWinner(carManager.createWinnerMessage());
-        } catch (NotBlankException e) {
-            printer.printExceptionMessage(e);
-            run();
-        }
-    }
-
-    private void proceedRound(int rounds) {
-        printer.printResultHeader();
-        for (int i = 0; i < rounds; i++) {
-            List<CarNumberDto> carNumberDtos = carManager.generateCarNumberDtos();
-            carManager.moveAllCars(carNumberDtos);
-            carManager.printCarState(printer);
-        }
-    }
-
-    private List<Car> createAllCars(List<String> carNames) {
-        List<Car> carList = new ArrayList<>();
-        if (carNames.size() == 0) {
-            throw new NotBlankException();
-        }
-
-        for (String name : carNames) {
-            carList.add(new Car(name));
-        }
-        return carList;
-    }
-
-    private int receiveRounds() {
-        int rounds = 0;
-        boolean isInvalidInput = true;
-        while (isInvalidInput) {
-            try {
-                rounds = receiver.receiveNumberOfRounds();
-                throwExceptionIfInputIsZero(rounds);
-                isInvalidInput = false;
-            } catch (InputMismatchException e) {
-                printer.printInputMismatchExceptionMessage(receiver);
-            } catch (IllegalArgumentException e) {
-                printer.printExceptionMessage(e);
-            }
-        }
-        return rounds;
-    }
-
-    private void throwExceptionIfInputIsZero(int rounds) {
-        if (rounds == INPUT_ZERO) {
-            throw new NotZeroRoundException();
-        }
-    }
-
-    private List<String> splitInputLine(String line) {
-        return List.of(line.split(DELIMITER));
+        Racing racing = new Racing(container, rounds);
+        Winner winners = racing.winners(printer);
+        printer.printWinner(winners);
     }
 
     public static void main(String[] args) {
