51d5c180de0172961271b7d1158f7d9b8ac95a4e
DongMin-Lee <pkalsh345@gmail.com>
2021-03-24 23:12:31 +0900

    refactor: 새로운 설계에 맞게 Printer 객체 구현

--- src/main/java/racingcar/ui/Printer.java
+++ src/main/java/racingcar/ui/Printer.java
@@ -1,13 +1,14 @@
 package racingcar.ui;
 
-import racingcar.domain.Car;
+import racingcar.domain.car.Cars;
+import racingcar.domain.Winner;
 
 public class Printer {
     private static final String CAR_NAME_REQUEST_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
     private static final String ROUND_REQUEST_MESSAGE = "시도할 회수는 몇회인가요?";
     private static final String RESULT_HEADER = "실행 결과";
     private static final String WINNER_MESSAGE = "가 최종 우승했습니다.";
-    private static final String INPUT_MISMATCH_EXCEPTION_MESSAGE = "라운드 횟수로 숫자만 입력하실 수 있습니다.";
+    private static final String INPUT_MISMATCH_EXCEPTION_MESSAGE = "라운드 입력으로 숫자만 입력할 수 있습니다.";
 
     public void requestCarName() {
         System.out.println(CAR_NAME_REQUEST_MESSAGE);
@@ -21,15 +22,12 @@ public class Printer {
         System.out.println(RESULT_HEADER);
     }
 
-    public void printWinner(String winnerNames) {
-        System.out.println(winnerNames + WINNER_MESSAGE);
+    public void printWinner(Winner winner) {
+        System.out.println(winner + WINNER_MESSAGE);
     }
 
-    public void printCarState(Car car) {
-        System.out.println(car);
-    }
-
-    public void printNewLine() {
+    public void printCarState(Cars cars) {
+        System.out.println(cars);
         System.out.println();
     }
 
@@ -37,8 +35,7 @@ public class Printer {
         System.out.println(e.getMessage());
     }
 
-    public void printInputMismatchExceptionMessage(Receiver receiver) {
-        receiver.clearBuffer();
+    public void printInputMismatchExceptionMessage() {
         System.out.println(INPUT_MISMATCH_EXCEPTION_MESSAGE);
     }
 }
