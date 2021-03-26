261fed235f11cfb693e92475af7a9ad44659abfb
DongMin-Lee <pkalsh345@gmail.com>
2021-03-26 15:29:03 +0900

    test: InputValidator에서 검증하는 문자열은 콤마만 연속하여 입력되는 경우로 한정하여 테스트 축소

--- src/test/java/racingcar/domain/round/RoundTest.java
+++ src/test/java/racingcar/domain/round/RoundTest.java
@@ -1,29 +1,24 @@
-package racingcar.factory;
+package racingcar.domain.round;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
-import racingcar.domain.round.Round;
-
-import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
-import static org.junit.jupiter.api.Assertions.*;
 
-class RoundFactoryTest {
+class RoundTest {
 
     @Test
-    void createRounds() {
+    void 정상적인_입력() {
         //given
         int expected = 5;
 
         //when
-        List<Round> rounds = RoundFactory.createRounds(5);
+        Round round = new Round(expected);
 
         //then
-        assertThat(rounds.size()).isEqualTo(expected);
-        rounds.forEach(round -> assertThat(round).isNotNull());
+        assertThat(round.getNumberOfRounds()).isEqualTo(expected);
     }
 
     @ParameterizedTest
@@ -31,7 +26,7 @@ class RoundFactoryTest {
     void 라운드_수가_0이하인_경우_예외(int numberOfRounds) {
         //when then
         assertThatExceptionOfType(IllegalArgumentException.class)
-                .isThrownBy(() -> RoundFactory.createRounds(numberOfRounds))
+                .isThrownBy(() -> new Round(numberOfRounds))
                 .withMessage("라운드 횟수는 1이상이어야 합니다.");
     }
 }
--- src/test/java/racingcar/ui/receiver/ReceiverTest.java
+++ src/test/java/racingcar/ui/receiver/ReceiverTest.java
@@ -53,17 +53,6 @@ class ReceiverTest {
                 .isThrownBy(receiver::receiveNumberOfRounds);
     }
 
-    @ValueSource(strings = {"0", "-1", "-20"})
-    @ParameterizedTest(name = "라운드 입력으로 0이하 숫자인 경우 [{index}] : input = {0}")
-    void 라운드_입력으로_0이하_숫자인_경우(String input) {
-        //given
-        Receiver receiver = new InputValidator(new ConsoleReceiver(new ByteArrayInputStream(input.getBytes())));
-
-        //when then
-        assertThatExceptionOfType(IllegalArgumentException.class)
-                .isThrownBy(receiver::receiveNumberOfRounds);
-    }
-
     @MethodSource(value = "validAfterInvalidRoundProvider")
     @ParameterizedTest(name = "라운드 입력으로 비정상적인 입력 후에 정상적인 입력이 들어오는 경우 [{index}]" +
             " : input = {0}, expected = {1}")
@@ -84,8 +73,8 @@ class ReceiverTest {
     private static Stream<Arguments> validAfterInvalidRoundProvider() {
         return Stream.of(
                 Arguments.of("s\n2", 2),
-                Arguments.of("-1\n3", 3),
-                Arguments.of("s\n-1\n4", 4)
+                Arguments.of("dsf\n3", 3),
+                Arguments.of("s\n*\n4", 4)
         );
     }
 
@@ -113,8 +102,8 @@ class ReceiverTest {
         );
     }
 
-    @ValueSource(strings = {",,", ",a,", "A,,,b"})
-    @ParameterizedTest(name = "비정상적인 Car 객체 이름 입력 [{index}] : input = {0}")
+    @ValueSource(strings = {",,", ",,,"})
+    @ParameterizedTest(name = "콤마만 존재하는 비정상적인 입력 [{index}] : input = {0}")
     void Car_객체_이름_비정상적인_입력(String input) {
         //given
         Receiver receiver = new InputValidator(new ConsoleReceiver(new ByteArrayInputStream(input.getBytes())));
@@ -122,7 +111,7 @@ class ReceiverTest {
         //when then
         assertThatExceptionOfType(IllegalArgumentException.class)
                 .isThrownBy(receiver::receiveCarNames)
-                .withMessage("자동차 이름의 입력은 콤마로 구분하여 하나 이상 입력해주세요.");
+                .withMessage("자동차 이름은 콤마만 입력할 수 없습니다.");
     }
 
     @MethodSource(value = {"validAfterInvalidCarNamesProvider"})
@@ -144,9 +133,9 @@ class ReceiverTest {
     private static Stream<Arguments> validAfterInvalidCarNamesProvider() {
         return Stream.of(
                 Arguments.of(",,\na,b,c\n", "a,b,c"),
-                Arguments.of(",a,\nab ed,v\n", "ab ed,v"),
-                Arguments.of("A,,,b\n^f%c,_car_\n", "^f%c,_car_"),
-                Arguments.of(",,\n,,b\ncar,caa,cab", "car,caa,cab")
+                Arguments.of(",,,\nab ed,v\n", "ab ed,v"),
+                Arguments.of(",,,\n^f%c,_car_\n", "^f%c,_car_"),
+                Arguments.of(",,\n,,,,\ncar,caa,cab", "car,caa,cab")
         );
     }
 }
