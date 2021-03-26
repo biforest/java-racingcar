35a136701a2fd9bac6c39a6e1eea96b95f859cdd
DongMin-Lee <pkalsh345@gmail.com>
2021-03-26 15:13:47 +0900

    test: 중복 이름 검증 테스트 이동
    - CarsTest -> CarFactoryTest

--- src/test/java/racingcar/domain/car/CarsTest.java
+++ src/test/java/racingcar/domain/car/CarsTest.java
@@ -2,27 +2,68 @@ package racingcar.domain.car;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import racingcar.strategy.MoveStrategy;
+import racingcar.strategy.ThresholdStrategy;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 class CarsTest {
 
-    @DisplayName(value = "같은 이름의 Car 객체 등록시 예외")
+    @DisplayName("가장 멀리 간 Car 객체의 이름들을 반환")
     @Test
-    void 같은_이름_등록시_예외() {
+    void getMaxPositionCarNames() {
         //given
-        List<Car> cars = new ArrayList<>();
-        cars.add(new Car("a"));
-        cars.add(new Car("b"));
-        cars.add(new Car("a"));
+        MoveStrategy strategy = new ThresholdStrategy(4, 4);
+        Car a = new Car("a").move(strategy);
+        Car b = new Car("b").move(strategy).move(strategy);
+        Car c = new Car("c").move(strategy).move(strategy);
 
-        //when, then
-        assertThatExceptionOfType(IllegalArgumentException.class)
-                .isThrownBy(() -> new Cars(cars))
-                .withMessage("중복되는 이름의 차를 입력하실 수 없습니다.");
+        Cars cars = new Cars(Arrays.asList(a, b, c));
+
+        //when
+        List<String> maxPositionCarNames = cars.getMaxPositionCarNames();
+
+        //then
+        assertThat(maxPositionCarNames).isEqualTo(Arrays.asList("b", "c"));
     }
 
+    @DisplayName("항상 움직이는 이동 전략과 항상 움직이지 않는 이동 전략을 한번씩 이용하여 Car 객체들을 이동")
+    @Test
+    void moveEachCar() {
+        //given
+        int expected = 1;
+        MoveStrategy forwardStrategy = new ThresholdStrategy(4, 4);
+        MoveStrategy stopStrategy = new ThresholdStrategy(10, 0);
+
+        Cars cars = new Cars(Arrays.asList(new Car("a"), new Car("b"), new Car("c")));
+
+        //when
+        cars = cars.moveEachCar(forwardStrategy);
+        cars = cars.moveEachCar(stopStrategy);
+
+        //then
+        Collection<Car> collection = cars.getCollection();
+        for (Car car : collection) {
+            assertThat(car.getPosition().getPosition()).isEqualTo(expected);
+        }
+    }
+
+    @DisplayName("Cars 객체의 불변성")
+    @Test
+    void immutabilityTest() {
+        //given
+        Cars cars = new Cars(Arrays.asList(new Car("a"), new Car("b"), new Car("c")));
+
+        //when
+        Cars newCars = cars.moveEachCar(new ThresholdStrategy(10, 0));
+
+        //then
+        assertThat(cars).isNotEqualTo(newCars);
+    }
 }
--- src/test/java/racingcar/factory/CarFactoryTest.java
+++ src/test/java/racingcar/factory/CarFactoryTest.java
@@ -5,7 +5,9 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 import racingcar.domain.car.Car;
+import racingcar.domain.car.Cars;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -28,9 +30,41 @@ class CarFactoryTest {
 
     @ValueSource(strings = {"pkalsh,a,b", "ff,fnv,fwoienf", "nvj,kbfkhsdf,dn"})
     @ParameterizedTest(name = "6자리 이상의 비정상적인 입력 [{index}] : input = {0}")
-    void 비정상적인_입력(String carNames) {
+    void 길이가_6자리_이상인_비정상적인_입력(String carNames) {
         assertThatExceptionOfType(IllegalArgumentException.class)
                 .isThrownBy(() -> CarFactory.createCars(carNames))
                 .withMessage("이름은 5자 이상을 넘길 수 없습니다.");
+    }
+
+    @ValueSource(strings = {",,b", ",dasd,"})
+    @ParameterizedTest(name = "빈 문자열이 존재하는 비정상적인 입력 [{index}] : input = {0}")
+    void 빈문자열이_존재하는_비정상적인_입력(String carNames) {
+        assertThatExceptionOfType(IllegalArgumentException.class)
+                .isThrownBy(() -> CarFactory.createCars(carNames))
+                .withMessage("이름은 공백일 수 없습니다.");
+    }
+
+    @DisplayName("콤마만 존재하는 비정상적인 입력")
+    @Test
+    void 콤마만_존재하는_비정상적인_입력() {
+        //given
+        String carNames = ",,,";
+
+        //when then
+        assertThatExceptionOfType(IllegalArgumentException.class)
+                .isThrownBy(() -> CarFactory.createCars(carNames))
+                .withMessage("콤마만 입력할 수는 없습니다.");
+    }
+
+    @DisplayName("같은 이름 입력시 예외")
+    @Test
+    void 같은_이름_등록시_예외() {
+        //given
+        String carNames = "a,b,a";
+
+        //when, then
+        assertThatExceptionOfType(IllegalArgumentException.class)
+                .isThrownBy(() -> CarFactory.createCars(carNames))
+                .withMessage("중복되는 이름의 차를 입력하실 수 없습니다.");
     }
 }
