17af58d504591f1444175c9ef70c94350d835432
DongMin-Lee <pkalsh345@gmail.com>
2021-03-26 15:31:03 +0900

    feat: 난수 생성 시작 숫자로 음수 사용시 예외

--- src/main/java/racingcar/strategy/ThresholdStrategy.java
+++ src/main/java/racingcar/strategy/ThresholdStrategy.java
@@ -4,19 +4,27 @@ import java.util.Random;
 
 public class ThresholdStrategy implements MoveStrategy {
     private static final int UPPER_BOUND = 10;
+    public static final String START_NUMBER_EXCEPTION_MESSAGE = "난수 생성은 양수만 가능합니다.";
 
     private final Random random;
     private final int threshold;
     private final int startNumber;
 
     public ThresholdStrategy(final int threshold, final int startNumber) {
+        validate(startNumber);
         random = new Random();
         this.threshold = threshold;
         this.startNumber = startNumber;
     }
 
+    private void validate(int startNumber) {
+        if (startNumber < 0) {
+            throw new IllegalArgumentException(START_NUMBER_EXCEPTION_MESSAGE);
+        }
+    }
+
     @Override
     public boolean isMove() {
-        return random.nextInt(UPPER_BOUND) + startNumber >= threshold;
+        return startNumber + random.nextInt(UPPER_BOUND - startNumber) >= threshold;
     }
 }
