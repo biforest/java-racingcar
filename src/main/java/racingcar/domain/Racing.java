2cd570101b7fd8c2b27218f39ec6f758c8483076
DongMin-Lee <pkalsh345@gmail.com>
2021-03-26 13:38:34 +0900

    refactor: Racing가 받는 메시지 세분화
    - start : 주어진 이동 전략으로 각각의 차 객체들을 움직이고 모든 라운드의 결과를 반환
    - winners : 마지막 라운드 결과에 대한 우승자를 Winner 객체로 반환

--- src/main/java/racingcar/domain/Racing.java
+++ src/main/java/racingcar/domain/Racing.java
@@ -1,23 +1,27 @@
 package racingcar.domain;
 
 import racingcar.domain.car.Cars;
-import racingcar.domain.round.Rounds;
-import racingcar.ui.Printer;
+import racingcar.domain.round.Round;
+import racingcar.strategy.MoveStrategy;
 
 import java.util.List;
 
 public class Racing {
-    private final Cars cars;
-    private final Rounds rounds;
+    private Cars cars;
+    private final Round round;
 
-    public Racing(Cars cars, Rounds rounds) {
+    public Racing(Cars cars, Round round) {
         this.cars = cars;
-        this.rounds = rounds;
+        this.round = round;
     }
 
-    public Winner winners(Printer printer) {
-        rounds.process(cars, printer);
-        List<String> maxPositionCarNames = cars.getMaxPositionCarNames();
-        return new Winner(maxPositionCarNames);
+    public List<Cars> start(MoveStrategy moveStrategy) {
+        List<Cars> results = round.process(cars, moveStrategy);
+        cars = results.get(results.size() - 1);
+        return results;
+    }
+
+    public Winner winners() {
+        return new Winner(cars.getMaxPositionCarNames());
     }
 }
