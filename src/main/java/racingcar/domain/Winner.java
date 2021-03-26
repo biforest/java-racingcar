8b733f77f973d9438551b50933a65ffcd2dfcacd
DongMin-Lee <pkalsh345@gmail.com>
2021-03-26 13:39:04 +0900

    refactor: StringJoiner에서 String.join으로 변경

--- src/main/java/racingcar/domain/Winner.java
+++ src/main/java/racingcar/domain/Winner.java
@@ -2,6 +2,7 @@ package racingcar.domain;
 
 import java.util.List;
 import java.util.StringJoiner;
+import java.util.stream.Collectors;
 
 public class Winner {
 
@@ -15,9 +16,6 @@ public class Winner {
 
     @Override
     public String toString() {
-        StringJoiner stringJoiner = new StringJoiner(DELIMITER);
-        winnerNames.forEach(stringJoiner::add);
-        return stringJoiner.toString();
+        return String.join(DELIMITER, winnerNames);
     }
-
 }
